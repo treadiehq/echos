8d3db03391dc60d3ba26fd6191d773fe38031bd2
Dante Lex <dantelex2@gmail.com>
2025-11-10 01:35:39 -0500

    Remove compressPublicAssets and add debug ls to build

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -3,8 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   devtools: { enabled: false },
   nitro: { 
-    preset: "node-server",
-    compressPublicAssets: true
+    preset: "node-server"
   },
   runtimeConfig: {
     public: {
